ba101d3b52b38a3217990bb936735440b93bc67b
LuisRcap95 <luisrcap95@gmail.com>
2025-05-19 00:30:54 -0600

    Preload image and edit letter message

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,10 @@
 const imageCart = document.querySelector('#card_btn');
 
+// Preload the image
+const preloadImage = new Image();
+preloadImage.src = './assets/media/cart_opened.png';
+
+
 imageCart.addEventListener('click', (e) => {
     e.preventDefault();
     imageCart.setAttribute('src', './assets/media/cart_opened.png');
